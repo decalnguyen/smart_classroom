1dc004cc70d3495eeeacd1a70f21fd2711e51bb1
decalnguyen <nnntoan.it@gmail.com>
2025-04-02 17:45:49 +0700

    Design frontend

--- auth-frontend/src/App.js
+++ auth-frontend/src/App.js
@@ -2,14 +2,22 @@ import React, { useState } from "react";
 import Login from "./components/Login";
 import SignUp from "./components/SignUp";
 import Dashboard from "./components/Dashboard";
+import Home from "./components/Home";
 import Nav from "./components/Nav";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
  return (
     <div className="App">
         <Nav />
       <main class="form-signin">
-        <Login />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/login" component={Login} />
+            <Route path="/signup" component={SignUp} />
+            <Route path="/home" exact component={Home} />
+          </Routes>
+        </BrowserRouter>
       </main>
     </div>
  );
