90fd266b72b81d9e911be002a0d95a9557170b41
decalnguyen <nnntoan.it@gmail.com>
2025-04-12 23:40:02 +0700

    Change frontend>

--- auth-frontend/src/App.js
+++ auth-frontend/src/App.js
@@ -7,24 +7,32 @@ import Classrooms from "./components/classrooms";
 import CalendarComponent from "./components/Calendar";
 import "./App.css";
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
-
+import { ColorModeContext, useMode } from "./theme";  
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import Topbar from "./components/global/Topbar";
 function AppContent() {
   const location = useLocation(); // Now inside BrowserRouter context
-
+  const [theme, colorMode] = useMode();
   return (
-    <div className="A">
-      {/* Conditionally render Nav based on the current path */}
-      {location.pathname !== "/classrooms" && <Nav />}
-      <main className="form-signin">
-        <Routes>
-          <Route path="/" exact element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/classrooms" element={<Classrooms />} />
-          <Route path="/calendar" element={<CalendarComponent />} />
-        </Routes>
-      </main>
-    </div>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}> 
+        <CssBaseline />
+        {/* Add a button to toggle between light and dark mode */}
+        <div className="app">
+          {/* Conditionally render Nav based on the current path */}
+          {location.pathname !== "/classrooms" && <Nav />}
+          <main className="content">
+            <Routes>
+              <Route path="/" exact element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<SignUp />} />
+              <Route path="/classrooms" element={<Classrooms />} />
+              <Route path="/calendar" element={<CalendarComponent />} />
+            </Routes>
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
---
+++ auth-frontend/src/components/global/Topbar.jsx
@@ -0,0 +1,15 @@
+import {Box, IconButton, useTheme} from "@mui/material";
+import {useContext} from "react";
+import {ColorModeContext, tokens} from "../../theme";
+import InputBase from "@mui/material";
+import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import NotificationsNoneOutlinedIcon from "@mui/icons-material/NotificationsNoneOutlined";
+import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
+import SearchIcon from "@mui/icons-material/Search";
+import PersonOutlinedIcon from "@mui/icons-material/SettingsOutlined";
+
+const Topbar = () => {
+    return <div>Topbar</div>
+}
+export default Topbar;
