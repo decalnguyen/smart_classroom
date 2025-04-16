c7aa848cc4f91f57357818c3b78be3d21326dee4
decalnguyen <nnntoan.it@gmail.com>
2025-04-16 11:49:11 +0700

    Frontend

--- auth-frontend/src/components/dashboard/dashboard.jsx
+++ auth-frontend/src/components/dashboard/dashboard.jsx
@@ -1,14 +1,63 @@
-import { Box } from "@mui/material";
+import { Box, Button, IconButton, Typography, useTheme } from "@mui/material";
 import Header from "../../components/Header";
+import { tokens } from "../../theme";
+import LineChart from "../charts/LineChart";
+import BarChart from "../charts/BarChart";
+import PieChart from "../charts/PieChart";
+import ProgressCircle from "../charts/ProgressCircle";
+import StatBox from "../charts/StatBox";
+import DownloadOutlinedIcon from "@mui/icons-material/DownloadOutlined";
+import EmailIcon from "@mui/icons-material/Email";
+import PointOfSaleIcon from "@mui/icons-material/PointOfSale";
+import PersonAddIcon from "@mui/icons-material/PersonAdd";
+import TrafficIcon from "@mui/icons-material/Traffic";
+import { DataGrid } from "@mui/x-data-grid";
+
 
 
 const Dashboard = () => {
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+
     return (
         <Box m="20px">
             <Box display="flex" justifyContent="space-between" alignItems="center">
                 <Header title="DASHBOARD" subtitle="Welcome to your dashboard" />
             </Box>
+            <Box>
+                <Button
+                    sx={{
+                        backgroundColor: colors.blueAccent[700],
+                        color: colors.grey[100],
+                        padding: "10px 20px",
+                        marginBottom: "20px",
+                    }}
+                >
+                    <DownloadOutlinedIcon sx={{ mr: "10px" }} />
+                    Download Report
+                </Button>
+                <Box display="grid" 
+                gridTemplateColumns="repeat(12, 1fr)" 
+                gridAutoRows="140px"
+                gap="20px">
+                    {/* ROW 1 */}
+                    <Box gridColumn="span 3" 
+                        backgroundColor={colors.primary[400]} 
+                        display="flex"
+                        alignItems="center"
+                        justifyContent="center"
+                    >
+                        <StatBox
+                            title="12,361"
+                            subtitle="Emails Sent"
+                            progress="0.75"
+                            increase="+14%"
+                            icon={<EmailIcon sx={{ color: colors.greenAccent[600], fontSize: "26px" }} />}
+                        />
+                    </Box>
+            </Box>
         </Box>
+    </Box>
     );
 };
 export default Dashboard;
